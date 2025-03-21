779f181dbc12b5254a06cd333e55d8bfb36f8cbf
David <davidcortesgil9@gmail.com>
2025-03-21 18:38:01 -0500

    Las clases se subieron

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+package Biblioteca;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola, estas en la biblioteca.");
+    }
+}
+
